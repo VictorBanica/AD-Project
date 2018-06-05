ed95e5f375fba141ebff12fc03641a501fc350e7
VictorBanica <32846243+VictorBanica@users.noreply.github.com>
2018-06-05 21:55:32 +0300

    Add files via upload

--- Rosensthiel.c
+++ Rosensthiel.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#define MAXNODES 25
+#include <time.h>
 
 struct list_node{
     int info;
@@ -64,7 +66,6 @@ int return_no_elements( struct list_node *head ){
     return no_elements;
 }
 
-
 void rosenstiehl( int number_of_nodes, int **adjacency_matrix, int root, struct list_node *head, struct list_node *cycle_head ) {
 
     int next_node;
@@ -86,8 +87,30 @@ void rosenstiehl( int number_of_nodes, int **adjacency_matrix, int root, struct 
     }
 }
 
+int test_for_cycle( int number_of_nodes, int **adjacency_matrix )
+{
+    int number_of_edges = 0;
+    int has_cycle = 1;
+    int iterator1, iterator2;
+
+    for(iterator1 = 0; iterator1 < number_of_nodes; iterator1++){
+        for(iterator2 = 0; iterator2 < number_of_nodes; iterator2++)
+            if(adjacency_matrix[iterator1][iterator2]==1)
+                number_of_edges++;
+
+        if(number_of_edges == 0 || number_of_edges % 2 != 0){
+            has_cycle = 0;
+            break;
+        }
+
+    number_of_edges = 0;
+    }
+    return has_cycle;
+}
+
 
 int main(){
+    srand(time(0));
 
     struct list_node *head = malloc(sizeof(struct list_node));
     head->next = NULL;
@@ -99,30 +122,90 @@ int main(){
     int **adjacency_matrix;
     int is_edge;
     int number_of_nodes;
+    int option;
 
-    printf("Enter the number of nodes in the graph: ");
-    scanf("%d", &number_of_nodes);
-    printf("\nThe nodes IDs range from 0 to %d\n", number_of_nodes - 1);
+    do{
 
-    adjacency_matrix = (int**)calloc( number_of_nodes, sizeof(int) );
-    for ( iterator1 = 0; iterator1 < number_of_nodes; iterator1++ )
-        adjacency_matrix[iterator1] = (int**)calloc( number_of_nodes, sizeof(int) );
-
-    for( iterator1 = 0; iterator1 < number_of_nodes; iterator1++){
-        for( iterator2 = 0; iterator2 < number_of_nodes; iterator2++ ){
-            scanf("%d", &is_edge);
-            if ( iterator1 == iterator2 )
-                adjacency_matrix[iterator1][iterator2] = 0;
-            else
-                adjacency_matrix[iterator1][iterator2] = is_edge;
-        }
+        printf("\n\n\nChoose an option: \n\n");
+        printf(" 1. Read input data manually, from keyboard \n\n");
+        printf(" 2. Generate random input data \n\n");
+        printf(" 3. Display the input data \n\n");
+        printf(" 4. Find eulerian cycle using Rosensthiel algorithm \n\n");
+        printf(" 5. Find eulerian cycle using Fleury algorithm \n\n");
+        printf(" 6. Exit program \n\n");
+        printf("\nYour option: ");
+        scanf("%i", &option);
         printf("\n\n");
-    }
 
-    int root = 0;
-    rosenstiehl(number_of_nodes, adjacency_matrix, root, head, cycle_head);
-    printf("\nThe eulerian cycle is: ");
-    print_list(cycle_head);
+        switch(option){
+            case 1:
+                printf("Enter the number of nodes in the graph: ");
+                scanf("%d", &number_of_nodes);
+                printf("\nThe nodes IDs range from 0 to %d\n", number_of_nodes - 1);
+
+                adjacency_matrix = (int**)calloc( number_of_nodes, sizeof(int) );
+                for ( iterator1 = 0; iterator1 < number_of_nodes; iterator1++ )
+                    adjacency_matrix[iterator1] = (int**)calloc( number_of_nodes, sizeof(int) );
+
+                printf("Input the adjacency matrix...\n\n");
+
+                for( iterator1 = 0; iterator1 < number_of_nodes; iterator1++){
+                    for( iterator2 = 0; iterator2 < number_of_nodes; iterator2++ ){
+                        scanf("%d", &is_edge);
+                        adjacency_matrix[iterator1][iterator2] = is_edge;
+                    }
+                printf("\n");
+                }
+                break;
+            case 2:
+
+                number_of_nodes = rand() % MAXNODES;
+
+                adjacency_matrix = (int**)calloc( number_of_nodes, sizeof(int) );
+                for ( iterator1 = 0; iterator1 < number_of_nodes; iterator1++ )
+                    adjacency_matrix[iterator1] = (int**)calloc( number_of_nodes, sizeof(int) );
+
+                for( iterator1 = 0; iterator1 < number_of_nodes; iterator1++ )
+                    for( iterator2 = 0; iterator2 < number_of_nodes; iterator2++ )
+                        if ( iterator1 != iterator2 )
+                           adjacency_matrix[iterator1][iterator2] = adjacency_matrix[iterator2][iterator1] = rand() % 2;
+                        else
+                           adjacency_matrix[iterator1][iterator2] = 0;
+
+                printf("\nThe random input data have been generated!");
+                break;
+
+            case 3:
+                printf("\nThe number of nodes is: %d\n\n", number_of_nodes);
+                printf("The adjacency matrix is: \n\n");
+                for(iterator1 = 0; iterator1 < number_of_nodes; iterator1++){
+                    for(iterator2 = 0; iterator2 < number_of_nodes; iterator2++)
+                        printf("%d ", adjacency_matrix[iterator1][iterator2]);
+                    printf("\n");
+                }
+                break;
+
+            case 4:
+                if(test_for_cycle(number_of_nodes, adjacency_matrix)){
+
+                    int root = 0;
+                    rosenstiehl(number_of_nodes, adjacency_matrix, root, head, cycle_head);
+                    printf("\nThe eulerian cycle is: ");
+                    print_list(cycle_head);
+                }else
+                    printf("\nThe graph has no eulerian cycle!");
+                break;
+
+            case 5:
+                printf("Fleury...");
+                break;
+            case 6:
+                return 0;
+            default:
+                printf("Wrong option!\n");
+            }
+    }while ( option != 6 );
+
 
     free(head);
     free(cycle_head);
